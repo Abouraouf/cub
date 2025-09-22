5a7c02539d96ef525a7738542905a022b05a0f9d
El Mahdi Abouraouf <eabourao@e2r13p1.1337.ma>
2025-09-22 18:17:28 +0100

    checking errors in the first lines

--- cub3D.c
+++ cub3D.c
@@ -6,121 +6,158 @@
 /*   By: eabourao <eabourao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 12:31:09 by eabourao          #+#    #+#             */
-/*   Updated: 2025/09/21 15:35:37 by eabourao         ###   ########.fr       */
+/*   Updated: 2025/09/22 17:59:03 by eabourao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+int	ft_check_duplicates(int	*flags)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < 5)
+	{
+		j = i + 1;
+		while (j < 5)
+		{
+			if (flags[i] == flags[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	find_nswe(char *str)
 {
-	if (ft_strncmp(str, "NO", 2))
+	if (!ft_strncmp(str, "NO ", 3))
 		return (1);
-	if (ft_strncmp(str, "SO", 2))
+	if (!ft_strncmp(str, "SO ", 3))
 		return (2);
-	if (ft_strncmp(str, "WE", 2))
+	if (!ft_strncmp(str, "WE ", 3))
 		return (3);
-	if (ft_strncmp(str, "EA", 2))
+	if (!ft_strncmp(str, "EA ", 3))
 		return (4);
-	if (ft_strncmp(str, "F", 2))
+	if (!ft_strncmp(str, "F ", 2))
 		return (5);
 	return (0);
 }
 
-void	trim_spaces(char *str)
+void	skip_spaces(char *str, int *i)
 {
 	if (!str)
 		return ;
-	while (*str && *str == ' ')
-		str++;
+	while (str[*i] && str[*i] == ' ')
+		(*i)++;
 }
 
 // this one checks the first line
-int	ft_check_first_lines(t_cub3d	*info)
-{
-	int	i;
-	int	ret;
+// int	ft_check_first_lines(t_cub3d	*info)
+// {
+// 	int	i;
+// 	int	ret;
 
-	ret = find_nswe(info->coord[i]);
-	i = 0;
-	while (info->coord[i])
-	{
-		if (!ret)
-			return (ret);
-	}
-}
+// 	ret = find_nswe(info->coord[i]);
+// 	i = 0;
+// 	while (info->coord[i])
+// 	{
+// 		if (!ret)
+// 			return (ret);
+// 	}
+// }
 
-char	*ft_read_first_lines(t_cub3d *info)
-{
-	int	one;
-	int	i;
-
-	i = 0;
-	while (info->coord[i])
-	{
-		trim_spaces(info->coord + i);
-		while (info->coord[i] && info->coord[i] != '\n')
-		{
-			
-		}
-		
-	}
-	
-}
-
-int	ft_check_first_lines_character(t_cub3d	*info)
+void	ft_copy_the_first_lines(t_cub3d *info, int i)
 {
 	
 }
 
-int	ft_check_first_lines_files(t_cub3d	*info)
-{
-	
-}
-
-void	ft_take_coord(t_cub3d *info)
+int	ft_check_first_lines(t_cub3d *info)
 {
 	int	i;
+	int	flag[5];
 	int	j;
-	int	flag;
 
-	if (!ft_check_first_lines(info) || !ft_check_first_lines_character(info)
-		|| !ft_check_first_lines(info))
-		return (NULL);
-	flag = 0;
-	j = 0;
 	i = 0;
-	while (info->map_coord[i])
+	j = 0;
+
+	while (j < 5)
 	{
-		if ()
+		skip_spaces(info->coord, &i);
+		flag[j] = find_nswe(info->coord + i);
+		if (!flag[j++])
+			return (info->error = 1, 0); 
+		while (info->coord[i] && info->coord[i] != '\n')
+			i++;
+		while (info->coord[i] && info->coord[i] == '\n')
+			i++;
 	}
+	ft_copy_the_first_lines(info, i);
+	return (ft_check_duplicates(flag));
 }
 
-int	parse_all(int fd, t_cub3d *info)
-{
-	ft_read_all (fd, info);
-	ft_take_coord(info);
-}
+// int	ft_check_first_lines_character(t_cub3d	*info)
+// {
+	
+// }
+
+// int	ft_check_first_lines_files(t_cub3d	*info)
+// {
+	
+// }
+
+// void	ft_take_coord(t_cub3d *info)
+// {
+// 	int	i;
+// 	int	j;
+// 	int	flag;
+
+// 	flag = 0;
+// 	j = 0;
+// 	i = 0;
+// 	// if (!ft_check_first_lines(info) || !ft_check_first_lines_character(info)
+// 	// 	|| !ft_check_first_lines_files(info))
+// 	// 	return (info->error = 1);
+// 	// while (info->map_coord[i])
+// 	// {
+// 	// 	if ()
+// 	// }
+// }
+
+// int	parse_all(int fd, t_cub3d *info)
+// {
+// 	ft_read_all (fd, info);
+// 	ft_take_coord(info);
+// }
 
 int	main(int ac, char **argv)
 {
+	(void) ac, (void) argv;
+	char *check = "NO 1 \n SO 2 \n WE 3 \n EA 4 \n F 5";
+	
 	t_cub3d	*info; //parse the second arg and init the info
-	int		fd;
+	// int		fd;
 
-	if (ac != 2)
-		return (printf("Error\n, wrong argument count\n"), 1);
+	// if (ac != 2)
+	// 	return (printf("Error\n, wrong argument count\n"), 1);
 
-	fd = open(argv[1], O_RDONLY);
+	// fd = open(argv[1], O_RDONLY);
 
-	if (fd < 0)
-		return (printf("Error\n"), 1);
+	// if (fd < 0)
+	// 	return (printf("Error\n"), 1);
 	info = malloc(sizeof(t_cub3d));
-	if (!info)
-	{
-		close(fd); 
-		return (printf("Error\n"), 1);
-	}
-	info->error = 0;
-	info->coord = NULL;
-	info->map_coord = NULL;
+	info->coord = check;
+	// if (!info)
+	// {
+	// 	close(fd); 
+	// 	return (printf("Error\n"), 1);
+	// }
+	// info->error = 0;
+	// info->coord = NULL;
+	// info->map_coord = NULL;
+	printf("\n%d\n", ft_check_first_lines(info));
 }
--- cub3D.h
+++ cub3D.h
@@ -6,7 +6,7 @@
 /*   By: eabourao <eabourao@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/15 12:29:55 by eabourao          #+#    #+#             */
-/*   Updated: 2025/09/21 15:28:06 by eabourao         ###   ########.fr       */
+/*   Updated: 2025/09/22 16:19:05 by eabourao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,10 @@ typedef struct s_cub3d
 	int		rows;
 	char	*map_coord;
 	char	*coord;
+	char	**first_lines;
 	int		error;
 }	t_cub3d;
+
 void	ft_read_all(int fd, t_cub3d *info);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 
